5010662f993933d680372010b7ddf5c26012858f
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-26 17:15:47 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_gopherci/ubuntu.py
+++ offregister_gopherci/ubuntu.py
@@ -2,8 +2,6 @@ import sys
 from functools import partial
 from os import path
 
-from fabric.context_managers import cd
-from fabric.operations import run
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.fs import cmd_avail
 from offregister_fab_utils.git import clone_or_update
@@ -19,12 +17,12 @@ hook_dir = partial(
 
 
 def install0(*args, **kwargs):
-    apt_depends("git")
+    apt_depends(c, "git")
     go.install0()
-    if not cmd_avail("glide"):
-        run("curl https://glide.sh/get | sh")
+    if not cmd_avail(c, "glide"):
+        c.run("curl https://glide.sh/get | sh")
 
-    gopath = run('echo "$GOPATH"', quiet=True)
+    gopath = c.run('echo "$GOPATH"', hide=True).stdout.rstrip()
 
     to_dir = "{gopath}/src/github.com/bradleyfalzon/gopherci".format(gopath=gopath)
     clone_or_update(
@@ -35,9 +33,9 @@ def install0(*args, **kwargs):
         skip_clean=True,
         skip_reset=True,
     )
-    with cd(to_dir):
-        run("glide i")
-        run("go build && mv gopherci $GOPATH/bin")
+    with c.cd(to_dir):
+        c.run("glide i")
+        c.run("go build && mv gopherci $GOPATH/bin")
 
     to_dir = "{gopath}/src/github.com/bradleyfalzon/gopherci-web".format(gopath=gopath)
     clone_or_update(
@@ -48,18 +46,16 @@ def install0(*args, **kwargs):
         skip_clean=True,
         skip_reset=True,
     )
-    with cd(to_dir):
-        run("glide i")
-        run("go build && mv gopherci-web {gopath}/bin".format(gopath=gopath))
+    with c.cd(to_dir):
+        c.run("glide i")
+        c.run("go build && mv gopherci-web {gopath}/bin".format(gopath=gopath))
 
     return {"installed": ("gopherci", "gopherci-web")}
 
 
-def services1(*args, **kwargs):
-    gopath = run('echo "$GOPATH"', quiet=True)
-    group, user = run(
-        """printf '%s:%s' "$USER" $(id -gn)""", shell_escape=False, quiet=True
-    ).split(":")
+def services1(c, *args, **kwargs):
+    gopath = c.run('echo "$GOPATH"', hide=True).stdout.rstrip()
+    group, user = c.run("""printf '%s:%s' "$USER" $(id -gn)""", hide=True).split(":")
     return {
         service: install_upgrade_service(
             service,
